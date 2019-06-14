98f48ae57089b0d6f28e48c6ccfabcaf8e10831a
iDataist <hwdgrmy@gmail.com>
2019-06-14 14:50:42 -0500

    Update etl.py

--- 3. Big-Data-with-Spark/etl.py
+++ 3. Big-Data-with-Spark/etl.py
@@ -23,19 +23,19 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = input_data + 'song_data'
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data/*.json)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select('song_id', 'title', 'artist_id', 'year', 'duration')
     
     # write songs table to parquet files partitioned by year and artist
     songs_table
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.select('artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude')
     
     # write artists table to parquet files
     artists_table
@@ -43,40 +43,40 @@ def process_song_data(spark, input_data, output_data):
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data =
+    log_data = input_data + 'log_data'
 
     # read log data file
-    df = 
+    df = spark.read.json(log_data/*json)
     
     # filter by actions for song plays
-    df = 
+    df = df.filter(df.page == 'NextSong')
 
     # extract columns for users table    
-    artists_table = 
+    users_table = df.select('userId', 'firstName', 'lastName', 'gender', 'level')
     
     # write users table to parquet files
-    artists_table
+    users_table
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
+    get_timestamp = udf(lambda x: datetime.fromtimestamp(x / 1000.0).time)
+    df = df.withColumn('timestamp', get_timestamp('df.ts'))
     
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    get_datetime = udf(lambda x: datetime.fromtimestamp(x / 1000.0))
+    df = df.withColumn('datetime', get_datetime('df.ts'))
     
     # extract columns to create time table
-    time_table = 
+    time_table = df.selectExpr('timestamp AS start_time', 'hour(datetime) AS hour', 'dayofmonth(datetime) AS day', 'weekofyear(datetime) AS week', 'month(datetime) AS month', 'year(datetime) AS year', 'date_format(datetime) AS weekday')
     
     # write time table to parquet files partitioned by year and month
     time_table
 
     # read in song data to use for songplays table
-    song_df = 
+    song_df = songs_table
 
     # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
-
+    songplays_table = pd.merge(song_df, df, how = 'right', left_on = ['title', 'duration'], right_on = ['song', 'length']).select('ts', 'userId', 'level','song_id', 'artist_id', 'sessionId', 'location', 'userAgent')
+ 
     # write songplays table to parquet files partitioned by year and month
     songplays_table
 
