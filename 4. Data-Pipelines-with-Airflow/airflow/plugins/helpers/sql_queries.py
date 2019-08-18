020ad6e6868cefa38d99bd6eeb721986de405080
iDataist <hwdgrmy@gmail.com>
2019-08-18 16:13:49 -0500

    Update sql_queries.py

--- 4. Data-Pipelines-with-Airflow/airflow/plugins/helpers/sql_queries.py
+++ 4. Data-Pipelines-with-Airflow/airflow/plugins/helpers/sql_queries.py
@@ -1,42 +1,57 @@
 class SqlQueries:
+    
     songplay_table_insert = ("""
-        SELECT
-                md5(events.sessionid || events.start_time) songplay_id,
-                events.start_time, 
-                events.userid, 
-                events.level, 
-                songs.song_id, 
-                songs.artist_id, 
-                events.sessionid, 
-                events.location, 
-                events.useragent
-                FROM (SELECT TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, *
-            FROM staging_events
-            WHERE page='NextSong') events
-            LEFT JOIN staging_songs songs
-            ON events.song = songs.title
-                AND events.artist = songs.artist_name
-                AND events.length = songs.duration
-    """)
+    (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) \
+    SELECT TIMESTAMP 'epoch' + e.ts/1000 * interval '1 second' AS start_time,  \
+    e.userId AS user_id,  \
+    e.level,  \
+    s.song_id,  \
+    s.artist_id,  \
+    e.sessionId AS session_id,  \
+    s.artist_location AS location,  \
+    e.userAgent AS user_agent \
+    FROM staging_songs s \
+    RIGHT JOIN staging_events e \
+    ON s.title = e.song \
+    AND s.artist_name = e.artist \
+    AND s.duration = e.length \
+    WHERE page = 'NextSong';""")
 
     user_table_insert = ("""
-        SELECT distinct userid, firstname, lastname, gender, level
-        FROM staging_events
-        WHERE page='NextSong'
-    """)
+    (user_id, first_name, last_name, gender, level) \
+    SELECT DISTINCT userId AS user_id,  \
+    firstName AS first_name,  \
+    lastName AS last_name,  \
+    gender,  \
+    level \
+    FROM staging_events
+    WHERE page = 'NextSong';""")
 
     song_table_insert = ("""
-        SELECT distinct song_id, title, artist_id, year, duration
-        FROM staging_songs
-    """)
+    (song_id, title, artist_id, year, duration) \
+    SELECT DISTINCT song_id,  \
+    title,  \
+    artist_id,  \
+    year,  \
+    duration \
+    FROM staging_songs;""")
 
     artist_table_insert = ("""
-        SELECT distinct artist_id, artist_name, artist_location, artist_latitude, artist_longitude
-        FROM staging_songs
-    """)
+    (artist_id, name, location, lattitude, longitude) \
+    SELECT DISTINCT artist_id,  \
+    artist_name AS name,  \
+    artist_location AS location,  \
+    artist_latitude AS lattitude,  \
+    artist_longitude AS longitude \
+    FROM staging_songs;""")
 
     time_table_insert = ("""
-        SELECT start_time, extract(hour from start_time), extract(day from start_time), extract(week from start_time), 
-               extract(month from start_time), extract(year from start_time), extract(dayofweek from start_time)
-        FROM songplays
-    """)
+    (start_time, hour, day, week, month, year, weekday) \
+    SELECT DISTINCT start_time, \
+    EXTRACT (hour from start_time) AS hour, \
+    EXTRACT (day from start_time)AS day, \
+    EXTRACT (week from start_time) AS week, \
+    EXTRACT (month from start_time) AS month, \
+    EXTRACT (year from start_time) AS year, \
+    EXTRACT (dayofweek from start_time) AS weekday \
+    FROM songplays;""")    
