9e4b5e93bf7f83911799a8c458cf4b1061ef8249
iDataist <hwdgrmy@gmail.com>
2019-08-18 16:14:02 -0500

    Update stage_redshift.py

--- 4. Data-Pipelines-with-Airflow/airflow/plugins/operators/stage_redshift.py
+++ 4. Data-Pipelines-with-Airflow/airflow/plugins/operators/stage_redshift.py
@@ -10,26 +10,27 @@ class StageToRedshiftOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id= '',
+                 aws_credentials_id = '', 
                  table_name ='',
                  s3_bucket='',
                  s3_key='',
-                 delimiter='',
+                 delimiter=',',
                  headers = 1, 
                  quote_char = '',
-                 file_type = 'csv', 
-                 aws_credentials_id = '', 
+                 file_type = '',            
                  *args, **kwargs):
 
-        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        self.table_name = table_name
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)        
         self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table_name = table_name
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.delimiter = delimiter
         self.headers = headers
         self.quote_char = quote_char
         self.file_type = file_type
-        self.aws_credentials_id = aws_credentials_id
+        
 
     def execute(self, context):
         aws_hook = AwsHook(self.aws_credentials_id)
@@ -39,26 +40,19 @@ class StageToRedshiftOperator(BaseOperator):
         self.log.info('Emptying stage table %s' % self.table_name)
         redshift.run('DELETE FROM %s' % self.table_name)
         
-        s3_path = 's3://%s/%s' % (self.s3_bucket, self.s3_key)
-        print(s3_path)       
+        s3_path = 's3://%s/%s' % (self.s3_bucket, self.s3_key)    
         copy_statement = """
                     COPY %s
                     FROM '%s'
                     access_key_id '%s'
                     secret_access_key '%s'
                     """ % (self.table_name, s3_path, credentials.access_key, credentials.secret_key)
-        
-        if self.file_type == 'csv': 
-            file_statement = """
-                        delimiter '%s'
-                        ignoreheader %s
-                        csv quote as '%s'; 
-                        """ % (self.delimiter, self.headers, self.quote_char)
-        if self.file_type == 'json':
-            file_statement = "json 'auto';"
+        if self.table_name == 'staging_events': 
+            file_statement = "json's3://udacity-dend/log_json_path.json';"
+        if self.table_name == 'staging_songs':
+            file_statement = "json'auto';"
         
         full_copy_statement = '%s %s' % (copy_statement, file_statement)
         self.log.info('Copying data from S3 to Redshift')
         redshift.run(full_copy_statement)
         self.log.info('Staging completed')
-
